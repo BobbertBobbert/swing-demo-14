e80808390afdb27235b3005a67ad5c16e2431284
BrobsterBob <s314829@student.salfordcc.ac.uk>
2025-03-27 16:28:59 +0000

    Added player sprite

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -7,4 +7,6 @@ public final class Constants {
 
     public static final int BOARD_WIDTH = 800;
     public static final int BOARD_HEIGHT = 600;
+
+    public static final String PLAYER_IMAGE_PATH = "resources/amogus.png";
 }
