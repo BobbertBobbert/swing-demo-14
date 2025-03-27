2c8ebf0e3ba4ad95f6cf1fa483201a3086b93ef8
BrobsterBob <s314829@student.salfordcc.ac.uk>
2025-03-27 16:16:25 +0000

    Added constants variables

--- src/Board.java
+++ src/Board.java
@@ -2,10 +2,14 @@ import javax.swing.*;
 import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 
+import static utils.Constants.BOARD_WIDTH;
+import static utils.Constants.BOARD_HEIGHT;
+
+
 public class Board extends JPanel {
 
     public Board() {
-        setPreferredSize(new Dimension(140, 140));
+        setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
         setBackground(ColorUIResource.black);
     }
 }
---
+++ src/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public final class Constants {
+    private Constants() {
+        // presents instantiation
+    }
+
+    public static final int BOARD_WIDTH = 800;
+    public static final int BOARD_HEIGHT = 600;
+}
